edfd6a3b02bf0110a3f55b97ced59ebb7c95c9e3
evenyag <realevenyag@gmail.com>
2023-03-03 19:35:58 +0800

    feat: Bench greptime

--- main.py
+++ main.py
@@ -25,7 +25,31 @@ class WorkspacePaths(object):
         self.binary_dir = '%s/bin' % workspace
         self.tsbs_binary_dir = '%s/tsbs' % self.binary_dir
         self.data_generator_path = '%s/tsbs_generate_data' % self.tsbs_binary_dir
-        self.tsbs_load_config_path = '%s/tsbs_load.yaml' % workspace
+        self.tsbs_load_greptime = '%s/tsbs_load_greptime.yaml' % workspace
+
+class BenchConfig(object):
+    def __init__(self, input_file):
+        self.file = input_file
+        self.urls = 'http://localhost:4000/v1/influxdb'
+        self.gzip = False
+        self.batch_size = '100'
+        self.workers = 4
+
+    def read_from_config(self, config):
+        loader = config['loader']
+        self.file = config['data-source']['file']['location']
+        self.urls = loader['db-specific']['urls']
+        self.gzip = loader['db-specific']['gzip']
+        self.batch_size = loader['runner']['batch-size']
+        self.workers = loader['runner']['workers']
+
+    def write_to_config(self, config):
+        loader = config['loader']
+        config['data-source']['file']['location'] = self.file
+        loader['db-specific']['urls'] = self.urls
+        loader['db-specific']['gzip'] = self.gzip
+        loader['runner']['batch-size'] = self.batch_size
+        loader['runner']['workers'] = self.workers
 
 def is_file_exists(path):
     file = Path(path)
@@ -54,28 +78,50 @@ def maybe_build_tsbs(paths):
 
 def generate_data(data_generator_path, output_file):
     cmd = '%s --use-case="cpu-only" --seed=123 --scale=4000 \
-        --timestamp-start="2016-01-01T00:00:00Z" \
-        --timestamp-end="2016-01-01T12:00:00Z" \
-        --log-interval="10s" --format="influx" > %s' % (data_generator_path, output_file)
+--timestamp-start="2016-01-01T00:00:00Z" \
+--timestamp-end="2016-01-01T12:00:00Z" \
+--log-interval="10s" --format="influx" > %s' % (data_generator_path, output_file)
     run_cmd(cmd, 'Generate data')
 
-def maybe_generate_tsbs_load_config(config_path, input_file):
+def load_yaml(path):
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)
+        return config
+
+def dump_yaml(config, path):
+    with open(path, 'w') as file:
+        yaml.dump(config, file)
+
+def config_tsbs_load_for_greptime(config_path, input_file):
     if is_file_exists(config_path):
-        return
+        # Config file already exists
+        config = load_yaml(config_path)
+
+        bench_config = BenchConfig(input_file)
+        # Read bench config from file
+        bench_config.read_from_config(config)
+
+        return bench_config
 
     # Load template
     template_path = '%s/tsbs_load.template.yaml' % CONFIG_DIR
-    with open(template_path, 'r') as file:
-        config = yaml.safe_load(file)
+    config = load_yaml(template_path)
 
-    # Overwrite template
-    config['data-source']['file'] = input_file
+    # Overwrite template by default config.
+    bench_config = BenchConfig(input_file)
+    bench_config.write_to_config(config)
 
     # Write yaml to config_path
-    with open(config_path, 'w') as file:
-        yaml.dump(config, file)
+    dump_yaml(config, config_path)
 
     print('generate config to %s' % config_path)
+
+    return bench_config
+
+def bench_greptime(tsbs_load_greptime, bench_config):
+    cmd = '%s --urls=%s --file=%s --batch-size=%s --gzip=%s --workers=%s' % (tsbs_load_greptime,
+        bench_config.urls, bench_config.file, bench_config.batch_size, bench_config.gzip, bench_config.workers)
+    run_cmd(cmd)
 
 def generate(args):
     paths = WorkspacePaths(args.workspace)
@@ -102,7 +148,9 @@ def greptime(args):
         input_file = '%s/%s' % (paths.workspace, args.input_name)
 
     print('input file is %s' % input_file)
-    maybe_generate_tsbs_load_config(paths.tsbs_load_config_path, input_file)
+    bench_config = config_tsbs_load_for_greptime(paths.tsbs_load_greptime, input_file)
+
+    bench_greptime(paths.tsbs_load_greptime, bench_config)
 
 if __name__ == '__main__':
     # Instantiate the parser
